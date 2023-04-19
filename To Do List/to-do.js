b82f934a1a71c6ada18558b39fe9c02074f699a5
Swathi <swathika.sidharth@gmail.com>
2023-04-19 09:50:00 -0400

    Added local Storage

--- To Do List/to-do.js
+++ To Do List/to-do.js
@@ -1,22 +1,43 @@
-const todoList = [];
+
+ // Initialize todoList array in local storage
+let todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+
+// Call function to display the todoList in page whenever we load.
 renderTodoList();
+
+// addToDO() function to do items to the list.
 function addToDo(){
-  let todoName = document.querySelector('.js-name-input').value;
-  let todoDate = document.querySelector('.js-date-input').value;
-  let todo = {name: todoName, dueDate:todoDate};
+  let todoName = document.querySelector('.js-name-input').value; // get input value from HTML through DOM query.
+  let todoDate = document.querySelector('.js-date-input').value; // get input value from HTML through DOM query.
+  let todo = {name: todoName, dueDate:todoDate}; // Save it as an object.
+
+  // Get the updated todoList from local storage and push the added item to it.
+  todoList = JSON.parse(localStorage.getItem('todoList')); 
   todoList.push(todo);
-  console.log(todoList);
+      localStorage.setItem('todoList', JSON.stringify(todoList));
+  //console.log(todoList);
+  // Make the input field blank after adding the item.
   document.querySelector('.js-name-input').value = '';
   document.querySelector('.js-date-input').value = '';
+
+  // Call function to display the todoList once it gets added.
   renderTodoList();
 }
 
+
+// Function to display the todoList once todo item gets added.
 function renderTodoList(){
-let todoListHTML = '';
-for (let i=0; i< todoList.length; i++){
-  const todoItem = todoList[i];
+let todoListHTML = ''; // initialize a variable to store HTML which will later be displayed in the page.
+let  temp = (JSON.parse(localStorage.getItem('todoList'))); // Get the updated todoList from the localStorage.
+//console.log(temp[0]);
+
+// Looping through the array of todoList to display all the items. 
+for (let i=0; i< temp.length; i++){
+  const todoItem = temp[i];
   const todoName = todoItem.name;
   const todoDate = todoItem.dueDate;
+
+  // Adding HTML elements in javascript to display the todo items once it gets added.
   const html = `
     <div>${todoName} </div>
     <div> ${todoDate} </div>
@@ -26,22 +47,27 @@ for (let i=0; i< todoList.length; i++){
     `;
   todoListHTML += html;
 }
-
+// Displaying the HTML elements from JS by accessing the <div>'s innerHTML.
 document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 
+// Event Listener for deleteButton to delete the Item once its clicked.
 document.querySelectorAll('.js-delete-todo-button'). 
       forEach((deleteButton, index) => {
           deleteButton.addEventListener('click' , () => {
-            todoList.splice(index, 1);  
-            renderTodoList();
+            let  todoDelete = (JSON.parse(localStorage.getItem('todoList'))); // get the Updated todoList from Local storage.
+            todoDelete.splice(index, 1);  // Deletes the item with index 'index'
+            localStorage.setItem('todoList', JSON.stringify(todoDelete)); // Update local Storage .
+            renderTodoList(); // Call the function to display the changes.
           });
         
       });
 }
 
+// Event Listener for add button which calls the addToDo() function when clicked.
 document.querySelector('.add-todo-button').
     addEventListener('click', () =>  {
       addToDo();
+     // localStorage.setItem('todoList', JSON.stringify(todo));
       }  
     );
 
