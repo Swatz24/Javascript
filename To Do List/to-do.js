e5105beefcbd8ad4ee7bbbfed3d1f64e7c42ec0c
Swathi <swathika.sidharth@gmail.com>
2023-04-19 14:38:56 -0400

    Added Local Storage for todolist

--- To Do List/to-do.js
+++ To Do List/to-do.js
@@ -1,8 +1,8 @@
 
  // Initialize todoList array in local storage
- let todoList =  [];
+ let todoList = JSON.parse(localStorage.getItem('todoList') || "[]");
 
-
+ renderTodoList();
 
  // Call function to display the todoList in page whenever we load.
  //renderTodoList();
@@ -14,10 +14,10 @@
    let todo = {name: todoName, dueDate:todoDate}; // Save it as an object.
  
    // Get the updated todoList from local storage and push the added item to it.
-  //  todoList = JSON.parse(localStorage.getItem('todoList')); 
-    todoList.push(todo);
-  //      localStorage.setItem('todoList', JSON.stringify(todoList));
-   //console.log(todoList);
+  //todoList = JSON.parse(localStorage.getItem('todoList')); 
+  todoList.push(todo);
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+   console.log(todoList);
    // Make the input field blank after adding the item.
    document.querySelector('.js-name-input').value = '';
    document.querySelector('.js-date-input').value = '';
@@ -39,16 +39,25 @@
    const todoName = todoItem.name;
    const todoDate = todoItem.dueDate;
  
-   // Adding HTML elements in javascript to display the todo items once it gets added.
-   const html = `
-     <div>${todoName} </div>
-     <div> ${todoDate} </div>
-     <button 
-       class="delete-todo-button js-delete-todo-button">Mark Completed
-       </button>
-     `;
-   todoListHTML += html;
- }
+   // Adding HTML elements in javascript to display the todo items once it gets added.(Added as <Div>s)
+//    const html = `
+//      <div>${todoName} </div>
+//      <div> ${todoDate} </div>
+//      <button 
+//        class="delete-todo-button js-delete-todo-button">Mark Completed
+//        </button>
+//      `;
+//    todoListHTML += html;
+//  }
+
+  // Adding HTML elements in javascript to display the todo items once it gets added.
+  //Edited to add as unordered list items.
+ const html = `<li class="todo-li"><span class ="liSpan">${todoName}. </span>
+                DueDate:${todoDate}      <button class="delete-todo-button js-delete-todo-button">Mark Completed
+   </button> </li>
+ `;
+todoListHTML += html;
+}
  // Displaying the HTML elements from JS by accessing the <div>'s innerHTML.
  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
  
@@ -56,9 +65,9 @@
  document.querySelectorAll('.js-delete-todo-button'). 
        forEach((deleteButton, index) => {
            deleteButton.addEventListener('click' , () => {
-            //  let  todoDelete = (JSON.parse(localStorage.getItem('todoList'))); // get the Updated todoList from Local storage.
+            let todoDelete = (JSON.parse(localStorage.getItem('todoList'))); // get the Updated todoList from Local storage.
              todoList.splice(index, 1);  // Deletes the item with index 'index'
-            //  localStorage.setItem('todoList', JSON.stringify(todoDelete)); // Update local Storage .
+            localStorage.setItem('todoList', JSON.stringify(todoList)); // Update local Storage .
              renderTodoList(); // Call the function to display the changes.
            });
          
